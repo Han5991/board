1013475136749d75413e0b85f719a2036e3e5575
Han Sang-Wook <73056306+Han5991@users.noreply.github.com>
2021-03-09 19:47:38 +0900

    [part3]~316p JPQL 사용 검색 기능 추가

--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -44,35 +44,48 @@ public class BoardRepositoryTests {
         System.out.println(board);
         System.out.println(board.getWriter());
     }
+
     @Test
-    public void testReadWithWriter(){
+    public void testReadWithWriter() {
         Object result = boardRepository.getBoardWithWriter(100L);
-        Object[]arr=(Object[]) result;
+        Object[] arr = (Object[]) result;
         System.out.println("-----------------------");
         System.out.println(Arrays.toString(arr));
     }
 
     @Test
-    public  void testGetBoardWithReply(){
+    public void testGetBoardWithReply() {
         List<Object[]> result = boardRepository.getBoardWithReply(100L);
-        for(Object[] arr : result)
+        for (Object[] arr : result)
             System.out.println(Arrays.toString(arr));
     }
 
     @Test
-    public void testWithReplyCount(){
-        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
-        Page <Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
-        result.get().forEach(row->{
+    public void testWithReplyCount() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
             Object[] arr = (Object[]) row;
             System.out.println(Arrays.toString(arr));
         });
     }
 
     @Test
-    public void testRead3(){
+    public void testRead3() {
         Object result = boardRepository.getBoardByBno(100L);
         Object[] arr = (Object[]) result;
         System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testSearch1() {
+        boardRepository.serch1();
+    }
+
+    @Test
+    public void testSearchPage() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending().and(Sort.by("title").ascending()));
+        Page<Object[]> result = boardRepository.searchPage("t", "1", pageable);
+        System.out.println(result);
     }
 }
